2ed84532b1d8dd39916ca9d1f8a73dfb2ddfb44b
Mlink6783 <mlink6783@gmail.com>
2025-04-30 23:19:12 +0600

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def bot_loop():
         except Exception as e:
             print(f"Bot error: {e}")
 
-        await asyncio.sleep(600)  # 10 minutes
+        await asyncio.sleep(100)  # 10 minutes
 
 
 @app.on_event("startup")
